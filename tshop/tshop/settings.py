3e1618461954d94f0fc7d840435914154bdcc948
belalahmed143 <mohammadbelalahmed82@gmail.com>
2021-12-10 00:12:57 +0600

    add heroku file

--- tshop/tshop/settings.py
+++ tshop/tshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ua9p4$^5hi310f$@o%)=*!d*9uox^+z$%-=)9cq8g08m**!-98
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' , 'tshopbangla.herokuapp.com']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
